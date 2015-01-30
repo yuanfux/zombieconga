3c1743a03fe314fd29ebf4fe837f4e2ad6c9625d
Yuan Fu <fuyuanx@gmail.com>
2015-01-30 15:56:15 -0800

    physics and scree sizes

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+	public float speed = -1;
+	private Transform spawnPoint;
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.velocity = new Vector2(speed, 0);
+		spawnPoint = GameObject.Find("SpawnPoint").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnBecameInvisible()
+	{
+		if (Camera.main == null)
+			return;
+		float yMax = Camera.main.orthographicSize - 0.5f;
+		transform.position = new Vector3( spawnPoint.position.x, 
+		                                 Random.Range(-yMax, yMax), 
+		                                 transform.position.z );
+	}
+}
--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -6,6 +6,9 @@ public class ZombieController : MonoBehaviour {
 	private Vector3 moveDirection;
 	public float turnSpeed;
 	// Use this for initialization
+	[SerializeField]
+	private PolygonCollider2D[] colliders;
+	private int currentColliderIndex = 0;
 	void Start () {
 		moveDirection = Vector3.right;
 	}
@@ -28,5 +31,41 @@ public class ZombieController : MonoBehaviour {
 		transform.position = Vector3.Lerp (currentPosition, target, Time.deltaTime);
 		float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.Slerp( transform.rotation, Quaternion.Euler( 0, 0, targetAngle ), turnSpeed * Time.deltaTime );
+		EnforceBounds ();
+	}
+
+	public void SetColliderForSprite( int spriteNum )
+	{
+		colliders[currentColliderIndex].enabled = false;
+		currentColliderIndex = spriteNum;
+		colliders[currentColliderIndex].enabled = true;
+	}
+
+	void OnTriggerEnter2D( Collider2D other )
+	{
+		Debug.Log ("Hit " + other.gameObject);
+	}
+	private void EnforceBounds()
+	{
+		Vector3 newPosition = transform.position; 
+		Camera mainCamera = Camera.main;
+		Vector3 cameraPosition = mainCamera.transform.position;
+
+		float xDist = mainCamera.aspect * mainCamera.orthographicSize; 
+		float xMax = cameraPosition.x + xDist;
+		float xMin = cameraPosition.x - xDist;
+
+		if ( newPosition.x < xMin || newPosition.x > xMax ) {
+			newPosition.x = Mathf.Clamp( newPosition.x, xMin, xMax );
+			moveDirection.x = -moveDirection.x;
+		}
+		float yMax = mainCamera.orthographicSize;
+		
+		if (newPosition.y < -yMax || newPosition.y > yMax) {
+			newPosition.y = Mathf.Clamp( newPosition.y, -yMax, yMax );
+			moveDirection.y = -moveDirection.y;
+		}
+
+		transform.position = newPosition;
 	}
 }
